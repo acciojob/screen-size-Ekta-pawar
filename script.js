07956a449f077d36dfc8a92bd0d93e807449bfdf
Ekta-pawar <ektapawar0001@gmail.com>
2025-07-28 11:06:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+    const sizeInfo = document.getElementById('sizeInfo');
+    const h1 = sizeInfo.querySelector('h1');
+
+    function updateSize() {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      h1.textContent = `Width: ${width} and Height: ${height}`;
+    }
+
+    window.addEventListener('resize', updateSize);
+    updateSize(); // Initialize on page load
